a5b7a8ef6b333692f7cffeb306f2881f29c92430
mvciekzvbek <maciej.jan.zabek@gmail.com>
2020-10-04 19:29:51 +0200

    init

---
+++ pages/about.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const HomePage: React.FunctionComponent = () => {
+  return <div>About</div>
+}
+
+export default HomePage
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const HomePage: React.FunctionComponent = () => {
+  return <div>Homepage</div>
+}
+
+export default HomePage
